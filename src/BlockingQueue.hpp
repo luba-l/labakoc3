84e61aefac099a54948bd47d39555b59d6aa1f1f
MariaBelyakovaa <mariya.belyakova.06@bk.ru>
2026-01-16 15:36:31 +0300

    Добавлен механизм завершения очереди

--- src/BlockingQueue.hpp
+++ src/BlockingQueue.hpp
@@ -1,4 +1,4 @@
-#pragma once
+ #pragma once
 #include <queue>
 #include <mutex>
 #include <condition_variable>
@@ -9,22 +9,36 @@ private:
     std::queue<T> queue_;
     std::mutex mutex_;
     std::condition_variable cv_;
+    bool finished_ = false;
 
 public:
     void push(const T& value) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        queue_.push(value);
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            queue_.push(value);
+        }
         cv_.notify_one();
     }
 
     bool pop(T& value) {
         std::unique_lock<std::mutex> lock(mutex_);
         cv_.wait(lock, [&] {
-            return !queue_.empty();
+            return !queue_.empty() || finished_;
         });
+
+        if (queue_.empty())
+            return false;
 
         value = queue_.front();
         queue_.pop();
         return true;
+    }
+
+    void finish() {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            finished_ = true;
+        }
+        cv_.notify_all();
     }
 };
