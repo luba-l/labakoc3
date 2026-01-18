1a7c672672c4a6b7136fc7cca8613efbca71214b
Luba <lubalev08243@gmail.com>
2026-01-18 11:56:30 +0300

    Улучшен класс Produser: добавлена обработка ошибки открытия файла

--- src/Producer.hpp
+++ src/Producer.hpp
@@ -4,16 +4,25 @@
 
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
 class Producer {
 public:
     static void run(const std::string& filename,
-                    BlockingQueue<Record>& queue) {
+                    BlockingQueue<Record>& inputQueue) {
 
         std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Ошибка открытия файла\n";
+            inputQueue.finish();
+            return;
+        }
+
         std::string line;
 
         while (std::getline(file, line)) {
+            if (line.empty()) continue;
+
             std::stringstream ss(line);
             Record r;
 
@@ -22,7 +31,9 @@ public:
             std::getline(ss, r.patronymic, ';');
             std::getline(ss, r.phone, ';');
 
-            queue.push(r);
+            inputQueue.push(r);
         }
+
+        inputQueue.finish();
     }
 };
