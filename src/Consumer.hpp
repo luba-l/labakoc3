4b0a839614b6287b999b9a0a1290b4b9ecb576cd
maria-skriabina <gomezpank@gmail.com>
2026-01-19 19:18:29 +0300

    Улучшен класс Consumer: добавлена проверка ФИО на кириллицу и телефона на корректный формат, потокобезопасный вывод в консоль, обработка некорректных записей и передача валидных данных в очередь для Writer

--- src/Consumer.hpp
+++ src/Consumer.hpp
@@ -1,24 +1,67 @@
 #pragma once
+
 #include "BlockingQueue.hpp"
 #include "Record.hpp"
 
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
+#include <mutex>
+#include <sstream>
+
+inline std::mutex coutMutex; // для синхронизации вывода в консоль
 
 class Consumer {
 public:
-    static void run(BlockingQueue<Record>& queue, int id) {
+    static void run(BlockingQueue<Record>& inputQueue,
+                    BlockingQueue<Record>& outputQueue,
+                    int id) {
+
         Record r;
 
-        while (queue.pop(r)) {
-            std::cout << "Поток " << id
-                      << " обрабатывает "
-                      << r.surname << std::endl;
+        while (inputQueue.pop(r)) {
 
-            std::this_thread::sleep_for(
-                std::chrono::seconds(2)
-            );
+            // Проверка записи
+            if (!r.isValid()) {
+                {
+                    std::ostringstream oss;
+                    oss << "[Consumer " << id
+                        << "] Пропущена некорректная запись: "
+                        << r.surname << ";"
+                        << r.name << ";"
+                        << r.patronymic << ";"
+                        << r.phone << "\n";
+
+                    std::lock_guard<std::mutex> lock(coutMutex);
+                    std::cout << oss.str();
+                }
+                continue; // пропускаем запись
+            }
+
+            // Обработка
+            {
+                std::ostringstream oss;
+                oss << "[Consumer " << id
+                    << "] Обработка: " << r.surname << "\n";
+
+                std::lock_guard<std::mutex> lock(coutMutex);
+                std::cout << oss.str();
+            }
+
+            // Симуляция обработки
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+
+            // Кладём только валидные записи в очередь для Writer
+            outputQueue.push(r);
+
+            {
+                std::ostringstream oss;
+                oss << "[Consumer " << id
+                    << "] Готово: " << r.surname << "\n";
+
+                std::lock_guard<std::mutex> lock(coutMutex);
+                std::cout << oss.str();
+            }
         }
     }
 };
